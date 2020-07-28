cbf408711816233a29b4f58548ad94005103c07e
Christine Bussinger <christinebussingerdev@gmail.com>
2020-07-27 21:01:23 -0400

    project seed bug fix

--- api/data/seeds/02-projects.js
+++ api/data/seeds/02-projects.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('projects').insert([
-        {id: 1, user_id: '5', description: "read more often"},
-        {id: 2, user_id: '5', description: 'exercise twice a week'},
-        {id: 3, user_id: '5', description: 'meditate for 5 minutes a day'}
+        {id: 1, user_id: '4', description: "read more often"},
+        {id: 2, user_id: '4', description: 'exercise twice a week'},
+        {id: 3, user_id: '4', description: 'meditate for 5 minutes a day'}
       ]);
     });
 };

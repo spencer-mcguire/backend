071027e7f56c9993e5208ea03be61f7ae5067d00
Christine Bussinger <christinebussingerdev@gmail.com>
2020-07-29 22:42:52 -0400

    changed project model for descriptive helper funcs

--- api/data/routers/projects/projects-router.js
+++ api/data/routers/projects/projects-router.js
@@ -1,39 +1,51 @@
-// const express = require('express')
+const express = require('express')
 
-// const db = require('./projects-model')
-// const { delete } = require('../../dbConfig')
+const db = require('./projects-model')
+const db = require('../../dbConfig')
 
-// const project = express.Router()
+const project = express.Router()
 
-// // GET all of user's projects
-// project.get('/api/projects/:userId', auth(), async(req, res, next) => {
-//   try {
-//     db.findById(req.params.userId)
-//   }
-//   catch(err) {next(err)}
-// })
+// GET all of user's projects
+project.get('/api/projects/:userId', auth(), async(req, res, next) => {
+  try {
+    const projects = await db.allUsersProjects(req.params.userId)
 
-// // GET all of user's projects
-// project.get('/api/projects/:userId', auth(), async(req, res, next) => {
-//   try {
-//     db.findById(req.params.userId)
-//   }
-//   catch(err) {next(err)}
-// })
+  }
+  catch(err) {next(err)}
+})
 
-// // CREATE
-// project.post('/api/projects', async(req, res, next) => {
+// GET specific project of user
+project.get('/api/projects/:userId/:id', auth(), async(req, res, next) => {
+  try {
+    const userProject = await db.specificProject(req.params.userId, req.params.id)
 
-// })
+    res.json(userProject)
+  }
+  catch(err) {next(err)}
+})
 
-// // DELETE
-// project.delete('/api/projects/:userId/:id', async(req, res, next) => {
+// CREATE
+project.post('/api/projects/:userId', async(req, res, next) => {
+  try {
+    const newProject = await db.add({...req.body, user_id: req.params.userId})
 
-// })
+    res.json({msg: 'new project added'})
+  }
+  catch(err) {next(err)}
+})
 
-// module.exports = project
+// DELETE
+project.delete('/api/projects/:userId/:id', async(req, res, next) => {
+  try{
+    db.remove(req.params.userId, req.params.id)
+    res.status(204).json({msg: 'project deleted'})
+  }
+  catch(err) {next(err)}
+})
 
-// // goals
-// // title date id (delete, fetch, put, update)
+module.exports = project
 
-// // values has true/false
+// goals
+// title date id (delete, fetch, put, update)
+
+// values has true/false
